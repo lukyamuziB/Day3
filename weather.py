5cd9856e85f2e1f1e02affba086c5b8095ffff98
= <=>
2017-07-05 11:51:04 +0300

    included better error handling and more forecast option

--- weather.py
+++ weather.py
@@ -9,10 +9,12 @@ data= {}
 #gets integer, prompting retry on invalid input
 def get_option(notif):
 	number= int(input(notif))
-	while(number.isdigit()==False):
+	while(not isinstance(number,int)):
 		number= raw_input("Please pick a valid option:  ")
 	return int(number)
 
+print("\n")
+print("______________HELLO, WELCOME TO MY SIMPLE WEATHER FORECAST PROGRAM_____________\n\n\n\n\n")
 city= input("I would like the forcast for: ")
 print (city)
 
@@ -23,8 +25,29 @@ pref= PATH_FOR_FORECAST if pref is 2 else PATH_FOR_CURRENT
 
 reply= requests.get(pref, params=data)
 
-print (reply.url)
+# print ("your reply URL is" + reply.url)
+pref= get_int("1. Current or 2. Forecast\n")
 
-reply= (reply.json())["current"]
+if pref is 2:
+	data.update(get_forecast_options())
 
-print (city+ " is "+ reply["condition"]["text"]+ " today, at "+ str(reply["temp_c"])+" degrees. "+ "It feels more like "+ str(reply["feelslike_c"])+ " degress though!")
+pref= PATH_FOR_FORECAST if pref is 2 else PATH_FOR_CURRENT
+
+print("Getting weather forecast for "+city+"...")
+
+try:
+	reply= requests.get(pref, params=data)
+except Error.ConnectionError as e:
+	print ("The Service is currently down, please retry in a few seconds :)\nOr make sure you have a working internet connection")
+
+if not reply.status_code == requests.codes.ok:
+	print ("The Service is currently down, please retry in a few seconds :)")
+
+reply= (reply.json())["current"] if pref is PATH_FOR_CURRENT else (reply.json())["forecast"]["forecastday"] #extract current object
+
+if pref is PATH_FOR_CURRENT:
+	print( city+ " is "+ reply["condition"]["text"]+ " today, at "+ str(reply["temp_c"])+" degrees. "+ "It feels more like "+ str(reply["feelslike_c"])+ " degress though!")
+else:
+	for day in range(0, data["days"]):
+		print ("Tomorrow," if day is 0 else "Then the next day,",)
+print (city+ " will be "+ reply[day]["day"]["condition"]["text"]+ ", at "+ str(reply[day]["day"]["avgtemp_c"])+" degrees on average.")
