061919c54d07a300499239fa0ad3b6969290fa38
= <=>
2017-07-05 15:41:10 +0300

    Increased weather properties, removed redundant code

--- weather.py
+++ weather.py
@@ -1,53 +1,32 @@
 import requests
+import requests.exceptions as Error
 
+data= {}
 API_KEY= "bc7cb8fb59984a4d9a373932170507"
 CORE_URL= "http://api.apixu.com/v1/"
 PATH_FOR_CURRENT= CORE_URL+"current.json?key="+API_KEY
-PATH_FOR_FORECAST= CORE_URL+"forecast.json?key="+API_KEY
-data= {}
-
-#gets integer, prompting retry on invalid input
-def get_option(notif):
-	number= int(input(notif))
-	while(not isinstance(number,int)):
-		number= raw_input("Please pick a valid option:  ")
-	return int(number)
 
 print("\n")
-print("______________HELLO, WELCOME TO MY SIMPLE WEATHER FORECAST PROGRAM_____________\n\n\n\n\n")
-city= input("I would like the forcast for: ")
+print("______________HELLO, WELCOME TO MY SIMPLE WEATHER PROGRAM_____________\n\n")
+city= input("Input the City whose weather info you need: ")
 print (city)
 
 data["q"]= city
-
-pref= get_option("1. Current or 2. Forecast\n")
-pref= PATH_FOR_FORECAST if pref is 2 else PATH_FOR_CURRENT
-
-reply= requests.get(pref, params=data)
-
-# print ("your reply URL is" + reply.url)
-pref= get_int("1. Current or 2. Forecast\n")
-
-if pref is 2:
-	data.update(get_forecast_options())
-
-pref= PATH_FOR_FORECAST if pref is 2 else PATH_FOR_CURRENT
-
-print("Getting weather forecast for "+city+"...")
+reply= requests.get(PATH_FOR_CURRENT, params = data)
+print ("your reply URL is: " + reply.url+"\n")
+print("Getting weather forecast for "+city+"...\n")
 
 try:
-	reply= requests.get(pref, params=data)
+	reply= requests.get(PATH_FOR_CURRENT, params=data)
 except Error.ConnectionError as e:
-	print ("The Service is currently down, please retry in a few seconds :)\nOr make sure you have a working internet connection")
+	print ("The Service is currently down, please retry in a few seconds :)")
 
 if not reply.status_code == requests.codes.ok:
 	print ("The Service is currently down, please retry in a few seconds :)")
 
-reply= (reply.json())["current"] if pref is PATH_FOR_CURRENT else (reply.json())["forecast"]["forecastday"] #extract current object
+reply= (reply.json())["current"]
 
-if pref is PATH_FOR_CURRENT:
-	print( city+ " is "+ reply["condition"]["text"]+ " today, at "+ str(reply["temp_c"])+" degrees. "+ "It feels more like "+ str(reply["feelslike_c"])+ " degress though!")
-else:
-	for day in range(0, data["days"]):
-		print ("Tomorrow," if day is 0 else "Then the next day,",)
-print (city+ " will be "+ reply[day]["day"]["condition"]["text"]+ ", at "+ str(reply[day]["day"]["avgtemp_c"])+" degrees on average.")
+print( city + " is "+ reply["condition"]["text"]+ " today, at "+ str(reply["temp_c"])+" degrees. "+ "It feels more like "+ str(reply["feelslike_c"])+ " degress though!")
+print("The wind speed is at: "+ str(reply["wind_kph"])+ " Kilometers per hour\n" + "The pressure is at: "+ str(reply["pressure_mb"])+" millibars")
+print("The humdity percentage is at: "+ str(reply["humidity"])+ "%\n"+"Cloud coverage is at: "+ str(reply["cloud"])+"%")
+print("Today's Precipitation in millimeters is at: "+ str(reply["precip_mm"])+"\nGeneral wind Direction: "+ str(reply["wind_degree"])+ " " +reply["wind_dir"])
